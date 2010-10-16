9a538611eb9502024750c274913b750bd966b7ba
Justin Rhoades <justin.rhoades@gmail.com>
2010-10-16 12:00:55 -0700

    First XCode import. New View-based iPad only project.

---
+++ Classes/AmosAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AmosAppDelegate.h
+//  Amos
+//
+//  Created by Justin Rhoades on 10/16/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AmosViewController;
+
+@interface AmosAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+    AmosViewController *viewController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet AmosViewController *viewController;
+
+@end
+
---
+++ Classes/AmosViewController.h
@@ -0,0 +1,16 @@
+//
+//  AmosViewController.h
+//  Amos
+//
+//  Created by Justin Rhoades on 10/16/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AmosViewController : UIViewController {
+
+}
+
+@end
+
